240ced254825e949c3dacb127d96675e570ff710
opilane <opilane@HP-CZC2349HTR>
2023-04-11 11:24:49 +0300

    Add project files.

---
+++ Mein.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alamprogrammid
+{
+    internal class Mein
+    {
+        public static void Main(string[] args)
+        {
+
+            double aritm = Alamprogramm.Keskmine(2);
+            Console.WriteLine(aritm);
+
+            string kassid = Alamprogramm.Kassi_kalkulaator(25,35,'+');
+            Console.WriteLine(kassid);
+
+            Alamprogramm.Stars(20);
+
+            Alamprogramm.NStars(5);
+
+        }
+
+
+    }
+}
